0720803e6b6e389b4f348ddfde79c1393bb6ef2d
kumarawdhesh <kumarawdhesh17@gmail.com>
2020-07-13 12:07:06 +0530

    first commit

---
+++ 03-Ansible/00-Setup/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Ansible on Ubuntu Box 
+sudo apt-get update 
+sudo apt-get install ansible sshpass -y 
+
+
+# Install Ansible on CentOS Box, then Uncomment the following lines  
+#sudo yum install epel-repo
+#sudo yum install ansible sshpass -y 
+
